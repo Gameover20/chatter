33e36c7408d5a8f9d6cb71bc20171a435976dd28
Gameover20 <megaprogrammer196@gmail.com>
2016-06-08 17:32:01 +0430

    ss

--- bot.lua
+++ bot.lua
@@ -6,8 +6,8 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "208182902:AAHrJ8MYk2fsZsoOjYY4vhmw0BmFSEuoHgU" --التوكم هنا
-local You = 87946267 --خلي ايدي حسابك
+local bot_api_key = "203599016:AAH36BHCBDLnc3T8aPVANspsg05oI2x0UF4" --التوكم هنا
+local You = 135164371 --خلي ايدي حسابك
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
